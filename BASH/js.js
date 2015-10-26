c0c1d9ec16bddfc61131124bfebebfafc2e43707
kylegarrettwilson <LetItBe@c-67-201-245-154.reshall.wwu.edu>
2015-10-25 18:29:16 -0700

    work

---
+++ BASH/js.js
@@ -0,0 +1,11 @@
+
+$.jqIpLocation({
+    ip : '123.125.71.78',
+    success: function(location) {
+        location.countryName;
+        location.countryCode;
+
+        console.log(location);
+
+    }
+});
